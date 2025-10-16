d949453dfe56d6f03d0cdbf59ca4df64ae259aa2
Miguel <miguelangel5cebrian@gmail.com>
2025-10-16 20:43:33 +0200

    Actualización del tipo Book

--- types/literature/Book.ts
+++ types/literature/Book.ts
@@ -1,5 +1,7 @@
 import { OptionalId } from "mongodb";
 import { Writer_Short } from "./Writer.ts";
+import { Album_Short } from "../music/Album.ts";
+import { Song_Short } from "../music/Song.ts";
 
 export type BookDB = OptionalId<{
     title: string,
@@ -23,6 +25,8 @@ export type Peticion_Book = {
     year_of_publish?: number,
     description?: string, 
     writer: Writer_Short,
+    talked_about_in_song: Song_Short[],
+    talked_about_in_album: Album_Short[],
 }
 
 export type Book_Short = {
@@ -30,4 +34,5 @@ export type Book_Short = {
     title: string,
     cover?: string,
     year_of_publish?: number,
+    writer: Writer_Short,
 }
