8fc055f54e0ac7cb65dd64679a7f1475ac5e068c
Miguel <miguelangel5cebrian@gmail.com>
2025-10-16 20:42:25 +0200

    Actualización del tipo Event

--- types/history/Event.ts
+++ types/history/Event.ts
@@ -2,6 +2,8 @@ import { ObjectId, OptionalId } from "mongodb"
 import { Person, Person_Short } from "./Person.ts";
 import { Organization, Organization_Short } from "./Organization.ts";
 import { Date } from "./Date.ts";
+import { Album_Short } from "../music/Album.ts";
+import { Song_Short } from "../music/Song.ts";
 
 export type EventDB = OptionalId<{
     name: string,
@@ -27,6 +29,8 @@ export type Peticion_Event = {
     end_date?: Date,
     people_involved?: Person_Short[],
     organizations_involved?: Organization_Short[],
+    talked_about_in_song: Song_Short[],
+    talked_about_in_album: Album_Short[],
 }
 
 export type Event_Short = {
